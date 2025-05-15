5d7706e268e4e3df2317abf5e524c246c1ddb55b
caiqueduart <dcaique242@gmail.com>
2025-05-15 08:48:02 -0300

    feat(landing-page): início dos trabalhos com landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [ MatButton ],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-}
+export class AppComponent { }
