89a1cea2304c8f66e95ca4af30c572df78c84fe9
caiqueduart <dcaique242@gmail.com>
2025-05-16 21:16:48 -0300

    feat(showcase): introduz showcase

--- src/app/features/showcase/showcase.component.ts
+++ src/app/features/showcase/showcase.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'kay-showcase',
-  imports: [],
+    imports: [
+        MatButton
+    ],
   templateUrl: './showcase.component.html',
   styleUrl: './showcase.component.scss'
 })
