458844e67ed312ab85e76612bacdcee2a2f53acf
caiqueduart <dcaique242@gmail.com>
2025-05-16 21:26:12 -0300

    refactor(showcase): refatora showcase

--- src/app/features/showcase/showcase.component.ts
+++ src/app/features/showcase/showcase.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {MatButton} from '@angular/material/button';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'kay-showcase',
     imports: [
-        MatButton
+        MatButton,
+        NgOptimizedImage
     ],
   templateUrl: './showcase.component.html',
   styleUrl: './showcase.component.scss'
