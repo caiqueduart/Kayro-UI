21ce14ae6ce9decec7cecf989d3868c81d3dadfa
caiqueduart <dcaique242@gmail.com>
2025-05-18 14:40:36 -0300

    ''

---
+++ src/app/core/layout/components/sys-header/sub-components/header-navigation/header-navigation.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {NavCategoryComponent} from "../nav-category/nav-category.component";
+import {NavCategoryItemDirective} from "../../directives/nav-category-item.directive";
+import {RouterLink} from "@angular/router";
+
+@Component({
+  selector: 'kay-header-navigation',
+    imports: [
+        NavCategoryComponent,
+        NavCategoryItemDirective,
+        RouterLink
+    ],
+  templateUrl: './header-navigation.component.html',
+  styleUrl: './header-navigation.component.scss'
+})
+export class HeaderNavigationComponent {
+
+}
--- src/app/core/layout/components/sys-header/sys-header.component.ts
+++ src/app/core/layout/components/sys-header/sys-header.component.ts
@@ -5,6 +5,7 @@ import {RouterLink} from '@angular/router';
 import {MatTooltip} from '@angular/material/tooltip';
 import {NgOptimizedImage} from '@angular/common';
 import {MenuToggleThemeComponent} from './sub-components/menu-toggle-theme/menu-toggle-theme.component';
+import {HeaderNavigationComponent} from './sub-components/header-navigation/header-navigation.component';
 
 @Component({
   selector: 'header[kay-sys-header]',
@@ -14,7 +15,8 @@ import {MenuToggleThemeComponent} from './sub-components/menu-toggle-theme/menu-
         RouterLink,
         MatTooltip,
         NgOptimizedImage,
-        MenuToggleThemeComponent
+        MenuToggleThemeComponent,
+        HeaderNavigationComponent
     ],
   templateUrl: './sys-header.component.html',
   styleUrl: './sys-header.component.scss'
