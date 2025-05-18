ef88e6dd1dca9f7244791d6faa291d9612fb949d
caiqueduart <dcaique242@gmail.com>
2025-05-18 14:22:16 -0300

    ''

---
+++ src/app/core/layout/components/sys-header/sub-components/nav-toggle-theme/nav-toggle-theme.component.ts
@@ -0,0 +1,36 @@
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {MatSlideToggle} from "@angular/material/slide-toggle";
+import {MatTooltip} from "@angular/material/tooltip";
+import {HandleThemeService} from '../../../../../services/handle-theme.service';
+import {moonIcon, sunIcon} from '../../models/toggle-theme-button.model';
+
+@Component({
+  selector: 'kay-nav-toggle-theme',
+    imports: [
+        MatSlideToggle,
+        MatTooltip
+    ],
+  templateUrl: './nav-toggle-theme.component.html',
+  styleUrl: './nav-toggle-theme.component.scss'
+})
+export class NavToggleThemeComponent implements AfterViewInit {
+    constructor(private handleThemeService: HandleThemeService) {}
+
+    @ViewChild('toggleThemeButton', { read: ElementRef }) element: ElementRef | undefined;
+
+    ngAfterViewInit(): void {
+
+        if (this.element) {
+            const toggleThemeButton = this.element.nativeElement;
+            const switchIconOn = toggleThemeButton.querySelector('.mdc-switch__icon--on').firstChild;
+            const switchIconOff = toggleThemeButton.querySelector('.mdc-switch__icon--off').firstChild;
+
+            switchIconOn.setAttribute('d', moonIcon);
+            switchIconOff.setAttribute('d', sunIcon);
+        }
+    }
+
+    toggleTheme(): void {
+        this.handleThemeService.toggleTheme();
+    }
+}
--- src/app/core/layout/components/sys-header/sys-header.component.ts
+++ src/app/core/layout/components/sys-header/sys-header.component.ts
@@ -1,12 +1,11 @@
-import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {Component} from '@angular/core';
 import {NavCategoryComponent} from './sub-components/nav-category/nav-category.component';
 import {NavCategoryItemDirective} from './directives/nav-category-item.directive';
 import {RouterLink} from '@angular/router';
 import {MatSlideToggle} from '@angular/material/slide-toggle';
-import {HandleThemeService} from '../../../services/handle-theme.service';
 import {MatTooltip} from '@angular/material/tooltip';
-import {moonIcon, sunIcon} from './models/toggle-theme-button.model';
 import {NgOptimizedImage} from '@angular/common';
+import {NavToggleThemeComponent} from './sub-components/nav-toggle-theme/nav-toggle-theme.component';
 
 @Component({
   selector: 'header[kay-sys-header]',
@@ -16,30 +15,10 @@ import {NgOptimizedImage} from '@angular/common';
         RouterLink,
         MatSlideToggle,
         MatTooltip,
-        NgOptimizedImage
+        NgOptimizedImage,
+        NavToggleThemeComponent
     ],
   templateUrl: './sys-header.component.html',
   styleUrl: './sys-header.component.scss'
 })
-export class SysHeaderComponent implements AfterViewInit {
-
-    constructor(private handleThemeService: HandleThemeService) {}
-
-    @ViewChild('toggleThemeButton', { read: ElementRef }) element: ElementRef | undefined;
-
-    ngAfterViewInit(): void {
-
-        if (this.element) {
-            const toggleThemeButton = this.element.nativeElement;
-            const switchIconOn = toggleThemeButton.querySelector('.mdc-switch__icon--on').firstChild;
-            const switchIconOff = toggleThemeButton.querySelector('.mdc-switch__icon--off').firstChild;
-
-            switchIconOn.setAttribute('d', moonIcon);
-            switchIconOff.setAttribute('d', sunIcon);
-        }
-    }
-
-    toggleTheme(): void {
-        this.handleThemeService.toggleTheme();
-    }
-}
+export class SysHeaderComponent { }
