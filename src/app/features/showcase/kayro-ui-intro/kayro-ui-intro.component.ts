913d6aaff91434947369793efdc6443534f1d702
caiqueduart <dcaique242@gmail.com>
2025-05-17 21:52:24 -0300

    feat(showcase): componentiza a intro

---
+++ src/app/features/showcase/kayro-ui-intro/kayro-ui-intro.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {MatButton} from '@angular/material/button';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'kay-ui-intro',
+    imports: [
+        MatButton,
+        NgOptimizedImage
+    ],
+  templateUrl: './kayro-ui-intro.component.html',
+  styleUrl: './kayro-ui-intro.component.scss'
+})
+export class KayroUiIntroComponent {
+
+}
