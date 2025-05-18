e1d4940368380be6096898ee3b73f3fe598dd0f9
caiqueduart <dcaique242@gmail.com>
2025-05-17 21:39:38 -0300

    feat(showcase): implementa vantagens kayro ui

---
+++ src/app/features/showcase/kayro-ui-features/kayro-ui-features.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {MatIcon} from '@angular/material/icon';
+
+@Component({
+  selector: 'kay-ui-features',
+    imports: [
+        MatIcon
+    ],
+  templateUrl: './kayro-ui-features.component.html',
+  styleUrl: './kayro-ui-features.component.scss'
+})
+export class KayroUiFeaturesComponent {
+
+}
--- src/app/features/showcase/showcase.component.ts
+++ src/app/features/showcase/showcase.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {MatButton} from '@angular/material/button';
 import {NgOptimizedImage} from '@angular/common';
+import {KayroUiFeaturesComponent} from './kayro-ui-features/kayro-ui-features.component';
 
 @Component({
   selector: 'kay-showcase',
     imports: [
         MatButton,
-        NgOptimizedImage
+        NgOptimizedImage,
+        KayroUiFeaturesComponent
     ],
   templateUrl: './showcase.component.html',
   styleUrl: './showcase.component.scss'
