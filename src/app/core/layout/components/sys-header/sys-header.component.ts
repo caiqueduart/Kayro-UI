9a6375390f34cbf17be16c96291b84b6fa0c28bb
caiqueduart <dcaique242@gmail.com>
2025-05-18 14:50:13 -0300

    ''

--- src/app/core/layout/components/sys-header/sys-header.component.ts
+++ src/app/core/layout/components/sys-header/sys-header.component.ts
@@ -1,6 +1,4 @@
 import {Component} from '@angular/core';
-import {MatTooltip} from '@angular/material/tooltip';
-import {NgOptimizedImage} from '@angular/common';
 import {MenuToggleThemeComponent} from './sub-components/menu-toggle-theme/menu-toggle-theme.component';
 import {HeaderNavigationComponent} from './sub-components/header-navigation/header-navigation.component';
 import {MenuSocialComponent} from './sub-components/menu-social/menu-social.component';
@@ -8,8 +6,6 @@ import {MenuSocialComponent} from './sub-components/menu-social/menu-social.comp
 @Component({
   selector: 'header[kay-sys-header]',
     imports: [
-        MatTooltip,
-        NgOptimizedImage,
         MenuToggleThemeComponent,
         HeaderNavigationComponent,
         MenuSocialComponent
