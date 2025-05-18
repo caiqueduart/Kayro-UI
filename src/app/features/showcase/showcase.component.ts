b793f16b554eed8a03bb7d03fa0ee5388295c263
caiqueduart <dcaique242@gmail.com>
2025-05-17 21:53:02 -0300

    refactor(showcase): remoção de imports não utilizados

--- src/app/features/showcase/showcase.component.ts
+++ src/app/features/showcase/showcase.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
-import {MatButton} from '@angular/material/button';
-import {NgOptimizedImage} from '@angular/common';
 import {KayroUiFeaturesComponent} from './kayro-ui-features/kayro-ui-features.component';
 import {KayroUiIntroComponent} from './kayro-ui-intro/kayro-ui-intro.component';
 
 @Component({
   selector: 'kay-showcase',
     imports: [
-        MatButton,
-        NgOptimizedImage,
         KayroUiFeaturesComponent,
         KayroUiIntroComponent
     ],
