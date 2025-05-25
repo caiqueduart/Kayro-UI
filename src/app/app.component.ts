a8b4b817ede5f4896421743cf897df31507a945a
caiqueduart <dcaique242@gmail.com>
2025-05-24 23:55:38 -0300

    ''

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SysHeaderComponent } from './core/layout/sys-header/sys-header.component';
 import { SysFooterComponent } from './core/layout/sys-footer/sys-footer.component';
@@ -8,6 +8,7 @@ import {
     MatSidenavContent,
 } from '@angular/material/sidenav';
 import { SysSidenavComponent } from './core/layout/sys-sidenav/sys-sidenav.component';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
     selector: 'app-root',
@@ -23,4 +24,22 @@ import { SysSidenavComponent } from './core/layout/sys-sidenav/sys-sidenav.compo
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+    @ViewChild('systemSidenav') systemSidenav!: MatSidenav;
+
+    constructor(private breakpointObserver: BreakpointObserver) {
+        this.breakpointObserver
+            .observe(['(max-width: 768px)'])
+            .subscribe((result) => {
+                const isTabletScreen = result.matches;
+
+                if (!isTabletScreen && this.systemSidenav) {
+                    this.closeSystemSidenav();
+                }
+            });
+    }
+
+    closeSystemSidenav(): void {
+        this.systemSidenav.close().then();
+    }
+}
