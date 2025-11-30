c285cfc8cf958162836c172a7f4b0a5ff160283c
Caique Duarte <dcaique242@gmail.com>
2025-11-30 10:41:27 -0300

    feat: figurinhas

--- src/app/features/showcase/kayro-ui-demos/kayro-ui-demos.component.ts
+++ src/app/features/showcase/kayro-ui-demos/kayro-ui-demos.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { StickersComponent } from './components/stickers/stickers.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 @Component({
     selector: 'kay-ui-demos',
-    imports: [StickersComponent],
+    imports: [StickersComponent, WelcomeComponent],
     templateUrl: './kayro-ui-demos.component.html',
     styleUrl: './kayro-ui-demos.component.scss',
 })
