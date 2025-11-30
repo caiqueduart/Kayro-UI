8801a0eae887fc50764fc292b0a3923c113b69f9
Caique Duarte <dcaique242@gmail.com>
2025-11-30 20:58:18 -0300

    feat: welcome

---
+++ src/app/features/showcase/kayro-ui-demos/components/showcase-container/showcase-container.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kay-showcase-container',
+  imports: [],
+  templateUrl: './showcase-container.component.html',
+  styleUrl: './showcase-container.component.scss',
+})
+export class ShowcaseContainerComponent {
+
+}
--- src/app/features/showcase/kayro-ui-demos/kayro-ui-demos.component.ts
+++ src/app/features/showcase/kayro-ui-demos/kayro-ui-demos.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { StickersComponent } from './components/stickers/stickers.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
+import { ShowcaseContainerComponent } from './components/showcase-container/showcase-container.component';
 
 @Component({
     selector: 'kay-ui-demos',
-    imports: [StickersComponent, WelcomeComponent],
+    imports: [StickersComponent, WelcomeComponent, ShowcaseContainerComponent],
     templateUrl: './kayro-ui-demos.component.html',
     styleUrl: './kayro-ui-demos.component.scss',
 })
