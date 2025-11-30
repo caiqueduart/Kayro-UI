5535445478249e5bd9151a8c52c548a0c4e2ff2f
Caique Duarte <dcaique242@gmail.com>
2025-11-30 20:50:32 -0300

    feat: welcome

--- src/app/features/showcase/kayro-ui-demos/components/welcome/welcome.component.ts
+++ src/app/features/showcase/kayro-ui-demos/components/welcome/welcome.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
-  selector: 'kay-welcome',
-  imports: [],
-  templateUrl: './welcome.component.html',
-  styleUrl: './welcome.component.scss'
+    selector: 'kay-welcome',
+    imports: [NgClass],
+    templateUrl: './welcome.component.html',
+    styleUrl: './welcome.component.scss',
 })
-export class WelcomeComponent {
+export class WelcomeComponent implements AfterViewInit {
+    private _observer!: IntersectionObserver;
+    private readonly _host = inject(ElementRef);
+    protected startAnime = false;
 
+    ngAfterViewInit(): void {
+        this._observer = new IntersectionObserver(
+            ([entry]) => {
+                if (entry.isIntersecting) {
+                    this.startAnime = true;
+                    this._observer.disconnect();
+                }
+            },
+            { threshold: 0.6 }, // começa quando 100% do elemento estiver visível
+        );
+
+        this._observer.observe(this._host.nativeElement);
+    }
+
+    ngOnDestroy(): void {
+        this._observer?.disconnect();
+    }
 }
