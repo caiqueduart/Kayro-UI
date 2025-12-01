bb48e711c9b4b6d838ff03be8ceff239df3fd70a
Caique Duarte <dcaique242@gmail.com>
2025-11-30 21:22:59 -0300

    feat: showcase-container

--- src/app/features/showcase/kayro-ui-demos/components/showcase-container/showcase-container.component.ts
+++ src/app/features/showcase/kayro-ui-demos/components/showcase-container/showcase-container.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { MatIconButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatTooltip } from '@angular/material/tooltip';
 
 @Component({
-  selector: 'kay-showcase-container',
-  imports: [],
-  templateUrl: './showcase-container.component.html',
-  styleUrl: './showcase-container.component.scss',
+    selector: 'kay-showcase-container',
+    imports: [MatIconButton, MatIcon, MatTooltip],
+    templateUrl: './showcase-container.component.html',
+    styleUrl: './showcase-container.component.scss',
 })
 export class ShowcaseContainerComponent {
+    protected isLoaded = true;
 
+    protected reload(): void {
+        this.isLoaded = false;
+
+        setTimeout(() => {
+            this.isLoaded = true;
+        });
+    }
 }
