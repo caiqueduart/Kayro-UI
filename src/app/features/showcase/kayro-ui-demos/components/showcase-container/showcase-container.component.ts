04a014d665b3e1f99d7a2d80c8b890a8083b8282
Caique Duarte <dcaique242@gmail.com>
2025-11-30 22:42:50 -0300

    feat: welcome

--- src/app/features/showcase/kayro-ui-demos/components/showcase-container/showcase-container.component.ts
+++ src/app/features/showcase/kayro-ui-demos/components/showcase-container/showcase-container.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatTooltip } from '@angular/material/tooltip';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
     selector: 'kay-showcase-container',
-    imports: [MatIconButton, MatIcon, MatTooltip],
+    imports: [MatIconButton, MatIcon, MatTooltip, NgOptimizedImage],
     templateUrl: './showcase-container.component.html',
     styleUrl: './showcase-container.component.scss',
 })
 export class ShowcaseContainerComponent {
+    hideWatermark = input<boolean>(false);
     protected isLoaded = true;
 
     protected reload(): void {
